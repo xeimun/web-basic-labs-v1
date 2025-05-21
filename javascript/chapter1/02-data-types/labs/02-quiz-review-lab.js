7be1f6ae66df51ce8df95109eb2c3143a0bc6ea3
xeimun <hinote444@naver.com>
2025-05-21 13:14:09 +0900

    study: data-types 실습 문문제 풀이

---
+++ javascript/chapter1/02-data-types/labs/02-quiz-review-lab.js
@@ -0,0 +1,9 @@
+// [문제] 다음 중 결과가 true가 되는 경우는?
+// 주석에 결과를 예측하고, 이유를 설명해보세요.
+
+console.log(Boolean("0")); // true
+console.log(Boolean(0)); // false
+console.log(Boolean([])); // true
+console.log(Boolean({})); // true
+console.log(Boolean(undefined)); // false
+console.log(Boolean(null)); // false
--- javascript/chapter1/02-data-types/labs/03-quiz-review-lab.js
+++
@@ -1,9 +0,0 @@
-// [문제] 다음 중 결과가 true가 되는 경우는?
-// 주석에 결과를 예측하고, 이유를 설명해보세요.
-
-console.log(Boolean("0"));      // ?
-console.log(Boolean(0));        // ?
-console.log(Boolean([]));       // ?
-console.log(Boolean({}));       // ?
-console.log(Boolean(undefined));// ?
-console.log(Boolean(null));     // ?
