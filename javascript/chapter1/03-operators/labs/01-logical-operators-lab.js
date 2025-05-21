371bee692f192b7a2b05699ddfc0d50920fdf312
xeimun <hinote444@naver.com>
2025-05-21 13:15:42 +0900

    study: operators 실습 문제 풀이

--- javascript/chapter1/03-operators/labs/01-logical-operators-lab.js
+++ javascript/chapter1/03-operators/labs/01-logical-operators-lab.js
@@ -1,20 +1,20 @@
 // 아래 코드에서 각각의 연산 결과가 true인지 false인지 적어보세요.
 
-console.log(5 == "5");
-console.log(5 === "5");
-console.log(7 > 2 && 2 > 7);
-console.log(0 == false);
+console.log(5 == "5"); // true
+console.log(5 === "5"); // false
+console.log(7 > 2 && 2 > 7); // false
+console.log(0 == false); // true
 
 let x = false;
 let y = true;
 let z = false;
 
-console.log(x || (y && z));
-console.log((x || y) && z);
+console.log(x || (y && z)); // false
+console.log((x || y) && z); // false
 
 let a = 2;
 let b = 3;
 let c = 4;
 
 let result = a++ + --b * c--;
-console.log(result);
+console.log(result); // 10
